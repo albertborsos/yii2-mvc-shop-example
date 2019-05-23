13a75ab572f9dc43441c016daf1c876ed7c89f9e
Albert Borsos <albertborsos@me.com>
2019-05-23 13:22:11 +0200

    create category translations by default

--- modules/shop/services/category/CreateCategoryService.php
+++ modules/shop/services/category/CreateCategoryService.php
@@ -2,9 +2,14 @@
 
 namespace app\modules\shop\services\category;
 
+use app\models\Language;
 use app\modules\shop\components\Service;
 use app\modules\shop\domains\category\Category;
+use app\modules\shop\services\category\forms\CreateCategoryDataForm;
 use app\modules\shop\services\category\forms\CreateCategoryForm;
+use yii\base\Exception;
+use yii\helpers\Html;
+use yii\helpers\Json;
 
 /**
  * Class CreateCategoryService
@@ -19,12 +24,39 @@ class CreateCategoryService extends Service
 
         $model->setAttributes($this->form->attributes);
 
-        if ($model->save()) {
-            return $model->id;
+        $transaction = \Yii::$app->db->beginTransaction();
+
+        try {
+            if ($model->save()) {
+                foreach (array_keys(Language::allowed()) as $languageCode) {
+                    $this->createDefaultTranslations($model, $languageCode);
+                }
+                $transaction->commit();
+                return $model->id;
+            }
+        } catch (Exception $e) {
+            $transaction->rollBack();
+            throw $e;
         }
 
         $this->form->addErrors($model->getErrors());
 
         return false;
+    }
+
+    private function createDefaultTranslations(Category $model, $languageCode)
+    {
+        $form = new CreateCategoryDataForm($model, $languageCode, [
+            'description' => $model->name . ' description',
+        ]);
+
+        if ($form->validate()) {
+            $service = new CreateOrUpdateCategoryDataService($form);
+            if ($service->execute()) {
+                return;
+            }
+        }
+
+        throw new Exception(Json::errorSummary($form));
     }
 }
